a94aa9f9ee8803ac023329a7b2564f84d6a0d551
Gustavo França <gustavo.franca@mercadolivre.com>
2021-06-11 16:01:50 -0300

    chore: adiciona exceções customizadas

---
+++ src/main/java/com/example/desafioquality/domain/Exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package com.example.desafioquality.domain.Exceptions;
+
+public class ConflictException extends Exception {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
