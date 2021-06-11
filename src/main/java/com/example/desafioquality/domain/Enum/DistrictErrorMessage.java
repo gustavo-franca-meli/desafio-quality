19d278216f80e06f1fb24efb6b6b8e597a5ce0e6
Gustavo França <gustavo.franca@mercadolivre.com>
2021-06-11 16:00:58 -0300

    chore: adiciona mensagens de erros

---
+++ src/main/java/com/example/desafioquality/domain/Enum/DistrictErrorMessage.java
@@ -0,0 +1,6 @@
+package com.example.desafioquality.domain.Enum;
+
+public class DistrictErrorMessage {
+    public static final String NAME_IS_EMPTY = "The district cannot be empty.";
+    public static final String NAME_LENGTH_EXCEED = "The neighborhood length cannot exceed 45 characters.";
+}
